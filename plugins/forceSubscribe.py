23a5208e099c46b29f9349d16cdc2f2019b29cdf
kanrusa <70271000+kanrusa@users.noreply.github.com>
2020-09-28 10:54:48 +0700

    Update forceSubscribe.py

--- plugins/forceSubscribe.py
+++ plugins/forceSubscribe.py
@@ -25,15 +25,15 @@ def _onUnMuteRequest(client, cb):
             if cb.message.reply_to_message.from_user.id == user_id:
               cb.message.delete()
           except UserNotParticipant:
-            client.answer_callback_query(cb.id, text="❗ Join the mentioned 'channel' and press the 'UnMute Me' button again.", show_alert=True)
+            client.answer_callback_query(cb.id, text="❗ Silahkan join 'channel' dan tekan tombol 'UnMute Me'.", show_alert=True)
       else:
-        client.answer_callback_query(cb.id, text="❗ You are muted by admins for other reasons.", show_alert=True)
+        client.answer_callback_query(cb.id, text="❗ Kamu di mute karena alasan lain.", show_alert=True)
     else:
       if not client.get_chat_member(chat_id, (client.get_me()).id).status == 'administrator':
         client.send_message(chat_id, f"❗ **{cb.from_user.mention} is trying to UnMute himself but i can't unmute him because i am not an admin in this chat add me as admin again.**\n__#Leaving this chat...__")
         client.leave_chat(chat_id)
       else:
-        client.answer_callback_query(cb.id, text="❗ Warning: Don't click the button if you can speak freely.", show_alert=True)
+        client.answer_callback_query(cb.id, text="❗ Warning: Jangan klik kalau kamu tidak sedang di mute.", show_alert=True)
 
 
 
@@ -50,18 +50,18 @@ def _check_member(client, message):
       except UserNotParticipant:
         try:
           sent_message = message.reply_text(
-              "{}, you are **not subscribed** to my [channel](https://t.me/{}) yet. Please [join](https://t.me/{}) and **press the button below** to unmute yourself.".format(message.from_user.mention, channel, channel),
+              "{}, kamu **belum subscribe**  [channel bjrp rated](https://t.me/{}). silahkan [join channel](https://t.me/{}) dan setelah itu **tekan tombol unmute dibawah** agar kamu bisa mengirim pesan di grup.".format(message.from_user.mention, channel, channel),
               disable_web_page_preview=True,
               reply_markup=InlineKeyboardMarkup(
-                  [[InlineKeyboardButton("UnMute Me", callback_data="onUnMuteRequest")]]
+                  [[InlineKeyboardButton("UnMute Saya", callback_data="onUnMuteRequest")]]
               )
           )
           client.restrict_chat_member(chat_id, user_id, ChatPermissions(can_send_messages=False))
         except ChatAdminRequired:
-          sent_message.edit("❗ **I am not an admin here.**\n__Make me admin with ban user permission and add me again.\n#Leaving this chat...__")
+          sent_message.edit("❗ **Saya bukan admin.**\n__Jadikan saya admin dengan ban permision dan tambahkan saya lagi.\n#Leaving this chat...__")
           client.leave_chat(chat_id)
       except ChatAdminRequired:
-        client.send_message(chat_id, text=f"❗ **I am not an admin in @{channel}**\n__Make me admin in the channel and add me again.\n#Leaving this chat...__")
+        client.send_message(chat_id, text=f"❗ **Saya bukan admin di channel @{channel}**\n__jadikan saya admin dan tambahkan saya lagi.\n#Leaving this chat...__")
         client.leave_chat(chat_id)
 
 
